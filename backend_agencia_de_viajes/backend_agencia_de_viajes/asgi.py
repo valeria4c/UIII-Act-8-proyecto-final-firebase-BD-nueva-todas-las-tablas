2b6f0ffa657979be97c1b2df4e14d108d6266daa
a.23308051280606 <a.23308051280606@cbtis128.edu.mx>
2025-12-01 20:13:29 +0000

    Initial commit

--- backend_agencia_de_viajes/backend_agencia_de_viajes/asgi.py
+++ backend_agencia_de_viajes/backend_agencia_de_viajes/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mysite project.
+ASGI config for backend_agencia_de_viajes project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_agencia_de_viajes.settings')
 
 application = get_asgi_application()
